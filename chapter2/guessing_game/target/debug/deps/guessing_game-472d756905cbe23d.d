75bd26a9cff4687d95eb95584528035ae4e348a1
BRUTALMETHOD <brutalmethod@users.noreply.github.com>
2022-12-29 02:00:07 +0800

    finish chapter 2

---
+++ chapter2/guessing_game/target/debug/deps/guessing_game-472d756905cbe23d.d
@@ -0,0 +1,5 @@
+/Users/hj/git_project/rustwithme/chapter2/guessing_game/target/debug/deps/guessing_game-472d756905cbe23d.rmeta: src/main.rs
+
+/Users/hj/git_project/rustwithme/chapter2/guessing_game/target/debug/deps/guessing_game-472d756905cbe23d.d: src/main.rs
+
+src/main.rs:
---
+++ chapter2/guessing_game/target/debug/deps/guessing_game-a14e89594e647d8e.d
@@ -0,0 +1,5 @@
+/Users/hj/git_project/rustwithme/chapter2/guessing_game/target/debug/deps/guessing_game-a14e89594e647d8e: src/main.rs
+
+/Users/hj/git_project/rustwithme/chapter2/guessing_game/target/debug/deps/guessing_game-a14e89594e647d8e.d: src/main.rs
+
+src/main.rs:
