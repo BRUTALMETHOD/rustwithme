7c48c08690ee635785c0e90b04d423a9f14655e2
BRUTALMETHOD <brutalmethod@users.noreply.github.com>
2022-12-29 22:44:11 +0800

    cleanup

--- chapter1/cargo/target/debug/cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/hj/git_project/rustwithme/chapter1/cargo/target/debug/cargo: /Users/hj/git_project/rustwithme/chapter1/cargo/src/main.rs
--- chapter1/cargo/target/debug/deps/cargo-127967fa69ea3447.d
+++
@@ -1,5 +0,0 @@
-/Users/hj/git_project/rustwithme/chapter1/cargo/target/debug/deps/cargo-127967fa69ea3447: src/main.rs
-
-/Users/hj/git_project/rustwithme/chapter1/cargo/target/debug/deps/cargo-127967fa69ea3447.d: src/main.rs
-
-src/main.rs:
